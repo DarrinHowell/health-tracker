3c99209608d16371ebd1786d2b3112139abd9f36
Darrin Howell <dbhowell09@gmail.com>
2019-01-08 10:52:21 -0800

    finger exercises registering clicks

--- app/src/main/java/com/example/health_tracker/MainActivity.java
+++ app/src/main/java/com/example/health_tracker/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.health_tracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
+    public int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        counter++;
+        TextView homeView = findViewById(R.id.counterText);
+        homeView.setText("Keep clicking! Here is your click count: " + counter);
+
     }
 }
