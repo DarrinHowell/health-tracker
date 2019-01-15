a7ab5c434d14af118511f1aea0b93e20f002784d
Darrin Howell <dbhowell09@gmail.com>
2019-01-15 13:45:55 -0800

    building out Exercise classes; sitll need to tryto add to dB

---
+++ app/src/main/java/com/example/health_tracker/Exercise.java
@@ -0,0 +1,25 @@
+package com.example.health_tracker;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Exercise {
+    @PrimaryKey
+    public int uid;
+
+    @ColumnInfo(name = "title")
+    public String title;
+
+    @ColumnInfo(name = "quantity")
+    public String quantity;
+
+    @ColumnInfo(name = "description")
+    public String description;
+
+    @ColumnInfo(name = "time_stamp")
+    public String timeStamp;
+
+    public
+}
