53190fd64990b64a310b33920f5f6f6cd03d2f94
Darrin Howell <dbhowell09@gmail.com>
2019-01-15 10:55:05 -0800

    notifications working, moving to scheduling

--- app/src/main/java/com/example/health_tracker/NotificationInitializor.java
+++ app/src/main/java/com/example/health_tracker/NotificationInitializor.java
@@ -3,6 +3,10 @@ package com.example.health_tracker;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +18,8 @@ public class NotificationInitializor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_initializor);
+
+        createNotificationChannel();
     }
 
     public void onClickScheduleNotification(View view) {
@@ -28,6 +34,22 @@ public class NotificationInitializor extends AppCompatActivity {
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
         // notificationId is a unique int for each notification that you must define
-        notificationManager.notify(1, mBuilder.build());
+        notificationManager.notify(notificationId++, mBuilder.build());
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Make it Rain";
+            String description = "Time to Hydrate";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
