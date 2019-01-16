3b9a976df8bc879093da3fb8cc0ae43272c077f7
Darrin Howell <dbhowell09@gmail.com>
2019-01-16 07:46:39 -0800

    updated README with screen shots of features; fixed bug in stop watch and relabled stop button

--- app/src/main/java/com/example/health_tracker/ExcerciseDatabase.java
+++ app/src/main/java/com/example/health_tracker/ExcerciseDatabase.java
@@ -1,9 +1,9 @@
-package com.example.health_tracker;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Exercise.class}, version = 1)
-public abstract class ExcerciseDatabase extends RoomDatabase {
-    public abstract ExerciseDao exerciseDao();
-}
+//package com.example.health_tracker;
+//
+//import androidx.room.Database;
+//import androidx.room.RoomDatabase;
+//
+//@Database(entities = {Exercise.class}, version = 1)
+//public abstract class ExcerciseDatabase extends RoomDatabase {
+//    public abstract ExerciseDao exerciseDao();
+//}
