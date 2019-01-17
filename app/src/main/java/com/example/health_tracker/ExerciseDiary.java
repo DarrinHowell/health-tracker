4adcd1344f50cee9686c76283137474311d45f00
Darrin Howell <dbhowell09@gmail.com>
2019-01-17 13:48:40 -0800

    pinging my spring app and receciving back JSON

--- app/src/main/java/com/example/health_tracker/ExerciseDiary.java
+++ app/src/main/java/com/example/health_tracker/ExerciseDiary.java
@@ -3,11 +3,19 @@ package com.example.health_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -28,6 +36,7 @@ public class ExerciseDiary extends AppCompatActivity {
                 ExerciseDatabase.class, "exercise-diary").allowMainThreadQueries().build();
 
         renderExerciseToDB();
+        getExercisesFromServer();
 
     }
 
@@ -64,6 +73,32 @@ public class ExerciseDiary extends AppCompatActivity {
 
         finish();
         startActivity(getIntent());
+    }
+
+    public void getExercisesFromServer() {
+//        final TextView mTextView = (TextView) findViewById(R.id.text);
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://health-tracker-backend-dbh.herokuapp.com/exercises";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        System.out.println("Response is: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!" + error);
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
 }
